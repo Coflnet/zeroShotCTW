6792385f74aadb33ed8ea9f917870c175fd06592
Rudra Patel <rudrapatel26122004@gmail.com>
2024-06-10 15:50:03 -0400

    sp

--- routers/processor.py
+++ routers/processor.py
@@ -4,10 +4,10 @@ from typing import List
 
 router = APIRouter()
 
-@router.post('/text')
+@router.post('/texts')
 async def process_text(texts: List[str]) -> List[List[float]]:
     return [[1., .2]]
 
-@router.post('/image')
+@router.post('/images')
 async def process_image(images: List[UploadFile] = File(...)) -> List[List[float]]:
     return [[1., .2]]
