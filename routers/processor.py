4bac020e4c4e2e0b95a48bec06e00e71be3c5348
Rudra Patel <rudrapatel26122004@gmail.com>
2024-06-11 18:00:19 -0400

    proper naming

--- routers/processor.py
+++ routers/processor.py
@@ -2,20 +2,20 @@ from fastapi import APIRouter, UploadFile, File
 from pydantic import BaseModel
 from typing import List
 from PIL import Image
-from ZeroShotCTW.pipeline import process_images, process_texts
+from ZeroShotCTW import pipeline
 import io
 
 router = APIRouter()
 
 
-@router.post('/texts')
-async def process_text(texts: List[str]) -> List[List[float]]:
-    embeddings = process_texts(texts)
+@router.post('/texts', tags=[''])
+async def process_texts(texts: List[str]) -> List[List[float]]:
+    embeddings = pipeline.process_texts(texts)
     return embeddings.detach().cpu().tolist()
 
 
 @router.post('/images')
-async def process_image(images: List[UploadFile] = File(...)) -> List[List[float]]:
+async def process_images(images: List[UploadFile] = File(...)) -> List[List[float]]:
     pil_images = [Image.open(io.BytesIO(image.file.read())) for image in images]
-    embeddings = process_images(pil_images)
+    embeddings = pipeline.process_images(pil_images)
     return embeddings.detach().cpu().tolist()
